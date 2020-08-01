1fc3032092a5af68c596fe73c84ea604a703444c
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-08-01 11:20:59 +0000

    Add seeds file

--- hasura/seeds/1596280635803_todos.sql
+++ hasura/seeds/1596280635803_todos.sql
@@ -1,8 +1,10 @@
+TRUNCATE todos;
+
 INSERT INTO todos
 (id, text)
 VALUES
-(1, 'buy milk'),
-(2, 'do laundry'),
-(3, 'call mom');
+(1, 'Buy milk'),
+(2, 'Do laundry'),
+(3, 'Call mom');
 
 ALTER SEQUENCE todos_id_seq RESTART WITH 4;
